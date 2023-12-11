2da50a0a9eb480d24586813307b7d603ed3df3ca
Oddyti <2067438609@qq.com>
2023-12-11 21:39:13 +0800

    测试生日版本

---
+++ js/old_days.js
@@ -0,0 +1,20 @@
+function old_days(){
+    var content = [
+        {
+            date: '2023年9月3日',
+            text: '瓜呆送货上门',
+            image_src: './images/old_days/2023-9-3.png'    
+        },
+        {
+            date: '2020年10月4日',
+            text: '武汉 · 画',
+            image_src: './images/old_days/2020-10-4.jpg'    
+        }
+    ];
+
+    var index = parseInt(Math.random() * content.length);
+    console.log(index);
+    document.getElementById("old-days-date").innerHTML = content[index].date;
+    document.getElementById("old-days-text").innerHTML = content[index].text;
+    document.getElementById("old-days-img").src = content[index].image_src;
+}
